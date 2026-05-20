4f443153828b8210da56021c349dda66440c8565
dudha369 <duduha2010@gmail.com>
2026-05-20 20:44:50 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,29 @@ import { init, initData, viewport } from "@telegram-apps/sdk";
 
 import { NavigationBar } from "./components/NavigationBar/NavigationBar";
 import UserContext from "./context/UserContext";
-
 import { request } from "./api/api";
 import { isMobileDevice, WebApp } from "./api/telegram";
-
 import { getValidTheme } from "./getValidTheme";
-
 import type { IUser } from "./interfaces/IUser";
 
 export function App() {
   const [safeTop, setSafeTop] = useState(0);
+  const [telegramReady, setTelegramReady] = useState(false);
 
-  const {data: user, isLoading} = useQuery({
+  const { data: user, isLoading: userLoading } = useQuery({
     queryKey: ["user"],
     queryFn: async () => {
       const res = await request("users/get");
       return res.data;
     },
-    select: (data) => data.user as IUser
+    select: (data) => data.user as IUser,
+    enabled: telegramReady
   });
 
   useEffect(() => {
     const initTelegram = async () => {
       try {
         init();
-
         initData.restore();
 
         await viewport.mount();
@@ -39,55 +37,49 @@ export function App() {
           WebApp?.requestFullscreen();
         }
 
+        const updateSafeArea = () => {
+          const top =
+            (WebApp?.safeAreaInset?.top ?? 0) +
+            (WebApp?.contentSafeAreaInset?.top ?? 0);
+
+          setSafeTop(top);
+        };
+
         updateSafeArea();
+
+        WebApp?.onEvent("fullscreenChanged", updateSafeArea);
+        WebApp?.onEvent("safeAreaChanged", updateSafeArea);
       } catch (error) {
         console.error("Telegram init error:", error);
+      } finally {
+        setTelegramReady(true);
       }
     };
 
-    const updateSafeArea = () => {
-      const top =
-        (WebApp?.safeAreaInset?.top ?? 0) +
-        (WebApp?.contentSafeAreaInset?.top ?? 0);
-
-      setSafeTop(top);
-    };
-
     initTelegram();
-
-    WebApp?.onEvent('fullscreenChanged', updateSafeArea);
-    WebApp?.onEvent('safeAreaChanged', updateSafeArea);
-
-    return () => {
-      WebApp?.offEvent('fullscreenChanged', updateSafeArea);
-      WebApp?.offEvent('safeAreaChanged', updateSafeArea);
-    };
   }, []);
 
   useEffect(() => {
-    if (!isLoading) {
+    if (telegramReady && !userLoading) {
       WebApp?.ready();
     }
-  }, [isLoading]);
+  }, [telegramReady, userLoading]);
 
-  const theme = getValidTheme(WebApp?.themeParams, "telegram"); // TODO
+  const theme = getValidTheme(WebApp?.themeParams, "telegram");
 
   const contextValue = useMemo(() => {
     return {
       user,
-      isLoading
+      isLoading: !telegramReady || userLoading
     };
-  }, [user, isLoading]);
+  }, [user, telegramReady, userLoading]);
 
   return (
-    <UserContext.Provider value={ contextValue }>
+    <UserContext.Provider value={contextValue}>
       <div
         style={{ paddingTop: safeTop }}
-        className="h-screen flex justify-center items-center text-white text-3xl"
+        className="h-screen flex flex-col text-white"
       >
-        <header>
-        </header>
-
         <main className="flex-1 overflow-y-auto">
           <Outlet />
         </main>
